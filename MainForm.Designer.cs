13218c59a27a66dfc9d35be748ea9314ab42c30c
Julian <89937721+Juliiiian@users.noreply.github.com>
2024-05-27 20:55:24 +0200

    Setup of main window

---
+++ MainForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Rust_Dev_Tool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menu = new MenuStrip();
+            mainToolStripMenuItem = new ToolStripMenuItem();
+            settingsToolStripMenuItem = new ToolStripMenuItem();
+            panel1 = new Panel();
+            menu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menu
+            // 
+            menu.BackColor = SystemColors.ActiveCaption;
+            menu.Font = new Font("Arial", 12F);
+            menu.Items.AddRange(new ToolStripItem[] { mainToolStripMenuItem, settingsToolStripMenuItem });
+            menu.Location = new Point(0, 0);
+            menu.Name = "menu";
+            menu.Padding = new Padding(6, 4, 0, 2);
+            menu.RenderMode = ToolStripRenderMode.Professional;
+            menu.Size = new Size(800, 28);
+            menu.TabIndex = 1;
+            menu.Text = "menubar";
+            menu.ItemClicked += menuStrip1_ItemClicked_1;
+            // 
+            // mainToolStripMenuItem
+            // 
+            mainToolStripMenuItem.Name = "mainToolStripMenuItem";
+            mainToolStripMenuItem.Size = new Size(54, 22);
+            mainToolStripMenuItem.Text = "Main";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            settingsToolStripMenuItem.Size = new Size(77, 22);
+            settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(12, 40);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(776, 390);
+            panel1.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 442);
+            Controls.Add(panel1);
+            Controls.Add(menu);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Rust Dev Tool";
+            menu.ResumeLayout(false);
+            menu.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menu;
+        private ToolStripMenuItem mainToolStripMenuItem;
+        private ToolStripMenuItem settingsToolStripMenuItem;
+        private Panel panel1;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,20 @@
+namespace Rust_Dev_Tool
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked_1(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
